ec92659564101f16978c071ef988957e26fa8102
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-18 15:15:56 +0700

    ...

--- CodexField/setup.sh
+++ CodexField/setup.sh
@@ -30,17 +30,15 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "isSkipInvalidProxy": false,
+  "delayEachAccount": [1, 1],
+  "referralCode": "RF4E6Aowi6sD",
+  "doTasks": false
 }
+
 EOL
 }
 
@@ -55,7 +53,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    CodexField BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +113,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
